0867d478c297a2762f01a697043473c4d6556d08
MrCyberlord <amanjain2691@gmail.com>
2023-08-22 14:01:21 +0530

    mobile responsive revision one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footerItems}>
+    <footer className={styles.footer}>
       <div className={styles.iconContainer}>
         <a
           href="https://twitter.com/MrCyberlord"
